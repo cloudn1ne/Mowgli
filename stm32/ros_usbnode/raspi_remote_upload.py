42bb5b562e5b29cb07a55af805861fb7a06b7b01
cn <cn@warp.at>
2022-06-19 14:43:44 +0200

    added pio config to remote programm via ssh/openocd

---
+++ stm32/ros_usbnode/raspi_remote_upload.py
@@ -0,0 +1,22 @@
+#####################################################################
+# use this script to load your code via a STM Link connected to the
+# raspi in mowgli
+#####################################################################
+Import("env")
+
+host = env.GetProjectOption("custom_mowgli_host")
+user = env.GetProjectOption("custom_mowgli_user")
+
+
+def mytarget_callback(*args, **kwargs):
+    print("Hello PlatformIO!")
+    env.Execute("scp \"$BUILD_DIR/${PROGNAME}.bin\" " + user + "@" + host + ":/tmp/firmware.bin")        
+    env.Execute("scp ./remote_upload/*.cfg " + user + "@" + host + ":/tmp")    
+    env.Execute("ssh " + user + "@" + host + " /usr/bin/openocd -f /tmp/yardforce500.cfg  -f /tmp/prog.cfg")
+
+
+env.AddCustomTarget(
+    "Mowgli Remote Upload",
+    "$BUILD_DIR/${PROGNAME}.bin",
+    mytarget_callback
+)
